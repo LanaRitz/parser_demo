d4520ce2c2498b8ba4d4105a6385b15a04d915a2
LanaRitz <milanaritz45@gmail.com>
2023-11-10 05:33:26 +0300

    Add CLI functions

---
+++ cli_functions.h
@@ -0,0 +1,19 @@
+#ifndef PARSER_DEMO_CLI_FUNCTIONS_H
+#define PARSER_DEMO_CLI_FUNCTIONS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string InputString(const string& message)
+{
+    cout << message;
+
+    string input;
+    getline(cin, input);
+
+    return input;
+}
+
+#endif //PARSER_DEMO_CLI_FUNCTIONS_H
